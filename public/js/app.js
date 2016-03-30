edc93d5eb7a1e91d4c1d315b5e426475ce2d5582
unknown <z003jn9v@MD1HCHSC.ww300.siemens.net>
2016-03-30 14:05:23 +0300

    Add message to browser UI

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,8 @@ socket.on('connect', function() {
 socket.on('message', function(message) {
 	console.log('New Message:');
 	console.log(message.text);
+
+	jQuery('.messages').append('<p>' + message.text + '</p>');
 });
 
 //handles submitting of new message
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ io.on('connection', function(socket) {
 	console.log('User connected via socket.io!');
 	socket.on('message', function(message) {
 		console.log('Message received: ' + message.text);
-		socket.broadcast.emit('message', message);
+		io.emit('message', message);
 	});
 	socket.emit('message', {
 		text: 'Welcome to the chat application!'
