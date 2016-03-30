2c4065dea779698ff5d0576079fab0666d044888
unknown <z003jn9v@MD1HCHSC.ww300.siemens.net>
2016-03-30 13:48:35 +0300

    added custom message event

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+var socket = io();
+
+socket.on('connect', function() {
+	console.log('Connected to socket io server');
+})
+
+socket.on('message', function(message) {
+	console.log('New Message:');
+	console.log(message.text);
+});
