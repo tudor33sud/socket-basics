0e8ecf28b31ad555cffc84981c1ae3c160a25e43
unknown <z003jn9v@MD1HCHSC.ww300.siemens.net>
2016-03-30 13:58:32 +0300

    Add frontend form to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,17 @@ socket.on('message', function(message) {
 	console.log('New Message:');
 	console.log(message.text);
 });
+
+//handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event) {
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]');
+	socket.emit('message', {
+		text: $message.val()
+	});
+
+	$message.val('');
+});
